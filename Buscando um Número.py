c7dd8a646b09f59a21e74dabd3c91b6df01f1374
Cristian Barbosa <cristianbrandao2007@gmail.com>
2025-08-11 14:43:48 -0300

    Solução concluída do exercício Buscando um Número.py

---
+++ Buscando um Número.py
@@ -0,0 +1,6 @@
+def buscar_numero(numero, lista):
+    return numero in lista
+
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42, 999999,
+           5, 1000000000, 13, 101010, 7, 444, 9, 2, 13, 9]
+print(buscar_numero(42, numeros))
