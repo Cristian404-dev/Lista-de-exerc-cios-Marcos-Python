8de0a492138a6561df3619bb89597839ce2a360e
Cristian Barbosa <cristianbrandao2007@gmail.com>
2025-08-11 15:07:52 -0300

    Solução concluída do exercício Encontrando os Números Pares.py

---
+++ Encontrando os Números Pares.py
@@ -0,0 +1,6 @@
+def numeros_pares(lista):
+    return [n for n in lista if n % 2 == 0]
+
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42, 999999,
+           5, 1000000000, 13, 101010, 7, 444, 9, 2, 13, 9]
+print(numeros_pares(numeros))
